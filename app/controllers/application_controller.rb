1047a92c2afb914fc50f47575d067cd05f48c680
Jackson Gariety <personal@jacksongariety.com>
2013-01-06 01:13:18 -0800

    Profile link is now red.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
-  
+
 private
 
 def current_user
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   # GET /posts
   # GET /posts.json
   def index
