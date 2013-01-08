287f60fddc1543ae6e432c6d49aa203fad029b68
Jackson Gariety <jackson@organichackers.(none)>
2013-01-08 01:31:37 -0800

    Prepares for deployment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ThelistIo::Application.initialize!
+
+#Production
+ENV['RAILS_ENV'] ||= 'production'
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20130106071956) do
-ActiveRecord::Schema.define(:version => 20130106071835) do
 
   create_table "comments", :force => true do |t|
     t.text     "text"
