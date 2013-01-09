c85c929fc7c5e671fd4001c4546ffcad9aea0082
Jackson Gariety <personal@jacksongariety.com>
2013-01-09 02:37:56 -0800

    Adds CSS for suffrage and makes numbers of the <ol> vertically centered as well as its content. Fuckin' Jimi Hendrix @ 2:30 AM.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,31 @@
 //= require jquery
 
 $(function(){
-  $('style').html($('style').html().replace(/\(([^\)]+)\)/,'(max-width: ' + ($('h1').width() + $('.meta-header').width() + 60) + 'px)'))
+
+  var rtime = new Date(1, 1, 2000, 12,00,00),
+      timeout = false,
+      delta = 200,
+      mediaQueries =  $('style')
+
+  function setMediaQuery() {
+    mediaQueries.html(mediaQueries.html().replace(/\(([^\)]+)\)/,'(max-width: ' + ($('h1').width() + $('.meta-header').width() + 60) + 'px)'));
+  }
+
+  $(window).resize(function() {
+      rtime = new Date();
+      if (timeout === false) {
+          timeout = true;
+          setTimeout(resizeend, delta);
+      }
+  });
+
+  function resizeend() {
+      if (new Date() - rtime < delta) {
+          setTimeout(resizeend, delta);
+      } else {
+          timeout = false;
+          setMediaQuery()
+      }
+  }
+
 });
