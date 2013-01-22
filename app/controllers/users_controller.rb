b778930a58fdff7184df1d22cb3a4cabbff2e9fb
Jackson Gariety <jackson@organichackers.(none)>
2013-01-21 18:58:10 -0800

    Fixed bug with improperly formatted route from the signup commit.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,15 @@ class UsersController < ApplicationController
 	  @user = User.new
 	end
 
-
 	def create
-		@token = @token = params[:token]
-		
+		@token = params[:token]
+
 	  if User.where(:gift_token => @token).count == 0
 	  	@user = User.find_or_initialize_by_id(params[:user].merge(:gift_token => @token))
 	  else
 	  	render "new"
 	  end
-	  
+
 	  if @user.save
 
 	    redirect_to root_url
