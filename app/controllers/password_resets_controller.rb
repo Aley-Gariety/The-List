249df2b1bb316439197c6853a2c463e92b54f02e
Jackson Gariety <personal@jacksongariety.com>
2013-01-26 00:28:26 -0800

    Fixes password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -16,10 +16,17 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
+
+    password_salt = BCrypt::Engine.generate_salt
+
     @user = User.find_by_password_reset_token!(params[:id])
     if @user.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, :alert => "Password reset has expired."
-    elsif @user.update_attributes(params[:user])
+    elsif @user.update_attributes({
+        :password_hash => BCrypt::Engine.hash_secret(params[:user][:password], password_salt),
+  	  	:password_salt => password_salt
+      })
+
       redirect_to root_url, :notice => "Password has been reset!"
     else
       render :edit
