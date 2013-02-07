180571ebb4bf6671e5d27749a266d03ce43592de
ColbyAley <colby@aley.me>
2013-02-06 22:18:02 -0800

    fixes my code. I'm a n00b

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,5 +4,5 @@ class Request < ActiveRecord::Base
   validates_presence_of :email
   validates_presence_of :name
   validates_format_of :email, :with => /@/
-  validates :name, :exclusion => { :within => %w(viagra cialis online anal)
+  validates_exclusion_of :name, :in => %w( viagra cialis anal online )
 end
