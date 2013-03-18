6521e944a0b4330aaf497ef2be0f86666117a33e
ColbyAley <colby@aley.me>
2013-03-17 22:01:40 -0700

    It's the small things

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -23,4 +23,17 @@ module PostsHelper
 
     markdown.render(text).html_safe
   end
+  
+  def comment_count(post_id)
+  	amount = Comment.where(:post_id => post_id, :comment_type => 0).count
+  	
+  	if amount == 0
+  		"Leave a Comment"
+  	elsif amount == 1
+  		"View 1 Comment"
+  	else
+  		"View " + amount.to_s + " Comments"
+  	end
+  end
+  
 end
--- app/helpers/suggestions_helper.rb
+++ app/helpers/suggestions_helper.rb
@@ -1,2 +1,15 @@
 module SuggestionsHelper
+
+  def comment_count(post_id)
+  	amount = Comment.where(:post_id => post_id, :comment_type => 1).count
+  	
+  	if amount == 0
+  		"Leave a Comment"
+  	elsif amount == 1
+  		"View 1 Comment"
+  	else
+  		"View " + amount.to_s + " Comments"
+  	end
+  end
+
 end
