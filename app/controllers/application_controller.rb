fb8445d26c529f170384198b3369bbf44e7adae2
Jackson Gariety <personal@jacksongariety.com>
2013-03-18 00:16:49 -0700

    Removes the level detection from a before_filter.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :detect_level
 
-	before_filter :require_login, :detect_level
+	before_filter :require_login
 	before_filter :free_invites, :only => [:index]
 
 	private
